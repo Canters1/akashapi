aa6f7d66561c0bba081d505942dc81e4b8271199
AkashPak <akashp8@nycstudents.net>
2023-12-07 09:26:14 -0500

    d

---
+++ vite-project/JS Files/main.js
@@ -0,0 +1,17 @@
+
+
+
+const URL= "https://api.quotable.io/random";
+
+ async function getData(URL) {
+try {
+    const response = await fetch(URL);
+    const data = await response.json(); //makes the data into JSON object we can use
+document.getElementsByClassName(".apiread").textContent = data.content;
+} catch (error) {
+     console.log(error);
+}
+}
+getData(URL)
+
+
