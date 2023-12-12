c1bb5af7f65f8782c44939487ceef2507027b316
AkashPak <akashp8@nycstudents.net>
2023-12-12 09:24:16 -0500

    ;

--- vite-project/JS Files/main.js
+++ vite-project/JS Files/main.js
@@ -14,18 +14,48 @@ document.getElementsByClassName(".apiread").textContent = data.content;
 getData(URL); */
 
 /* const URL = "https://www.amiiboapi.com/api/" */
-const URL = "https://www.amiiboapi.com/api/amiibo/?name=Peach"
+
+
+
+
+
+
+
+/* const URL = "https://www.amiiboapi.com/api/amiibo/?name=Peach"
 async function getdata(URL){
     try{
         const res = await fetch(URL);
         console.log(res);
         const data= await res.json();
+        document.querySelector(".apiread").textContent = data.
         console.log(data);
     } catch(error) {
         console.log(error);
     }
 }
-getdata(URL)
+const amiibodata = getdata(URL) */
+
+const RL ="https://www.amiiboapi.com/api/";
+
+async function getData(URL){
+    try {
+        if(response.status !=100){
+            let amiibo= "Mario"
+            throw new Error(amiibo)
+        }
+    const response = await fetch(URL);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("h1").textContent= data.content;
+    document.querySelector("h2").textContent= data.author;
+    } catch(error){
+           console.log(error)
+    }
+}
+getData(RL);
+
+
 
 
 
