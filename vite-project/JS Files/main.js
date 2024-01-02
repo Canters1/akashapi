b6f9c950945b78cdc5fe57873216263f49361923
AkashPak <akashp8@nycstudents.net>
2024-01-02 09:26:10 -0500

    ds

--- vite-project/JS Files/main.js
+++ vite-project/JS Files/main.js
@@ -1,45 +1,37 @@
-const Url=`https://pokeapi.co/api/v2/pokemon/`;
+
 const DomSelectors={
 form:document.querySelector("#form"),
 h1s: document.querySelector(".h1"),
 h2s: document.querySelector(".h2"),
 amiiboname: document.querySelector(".amiibo-name")
 }
- 
-
-
-
-
-
-
-async function getData(URL){
+const Url= "https://www.amiiboapi.com/api/amiibo/?name=mario";
+async function getData(money) {
     try {
-        if(response.status !=character){
-            let amiibo= "Mario"
-            throw new Error(amiibo)
-        }
-    const response = await fetch(URL);
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    } catch(error){
-           console.log(error)
+        const response = await fetch(money);
+        const data = await response.json(); //makes the data into JSON object we can use
+        console.log(data)
+    } catch (error) {
+         console.log(error);
     }
-}
+    };
+    getData(Url)
+;
 function AddAmiibo() {
     document.querySelector(".flex-container").insertAdjacentHTML("beforeend",
     `
-    <div class="amiibocard">
-    <h1 class="title"><h1>
-    <h2 class="description">        <h2>
+    <div class="amiibocard">       <div>
+    <h1 class="title">             <h1>
+    <h2 class="description"><h2>
     `
     )
 };  
-getData(Url); 
 
+const Search=
 DomSelectors.form.addEventListener("submit",function(event){
     event.preventDefault();
     console.log(DomSelectors.amiiboname.value);
-     DomSelectors.form.addEventListener("submit", function(){
-        (el)=> el.textContent = DomSelectors.amiiboname.value})}
-);    
+
+DomSelectors.form.addEventListener("submit", function(){
+        (el)=> el.textContent = DomSelectors.h1s.value})}
+);   
