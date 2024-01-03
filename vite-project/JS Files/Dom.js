5f45b121f672ccf0fef112927f31039a5666b278
AkashPak <akashp8@nycstudents.net>
2024-01-03 08:52:21 -0500

    Create Dom.js

---
+++ vite-project/JS Files/Dom.js
@@ -0,0 +1,8 @@
+
+const DomSelectors={
+    form:document.querySelector("#form"),
+    h1s: document.querySelector(".h1"),
+    h2s: document.querySelector(".h2"),
+    amiiboname: document.querySelector(".amiibo-name")
+    }
+    export{DomSelectors}
