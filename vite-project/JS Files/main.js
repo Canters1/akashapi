42ff9773e6842eceaaf5c54826684b6fd5d2c370
AkashPak <144455859+AkashPak@users.noreply.github.com>
2024-01-06 21:23:36 -0500

    dawd

--- vite-project/JS Files/main.js
+++ vite-project/JS Files/main.js
@@ -1,55 +1,50 @@
+import { DomSelectors } from "/JS Files/Dom.js";
 
-import { DomSelectors } from "/JS Files/Dom.js"
-
-
-
-const Url= "https://www.amiiboapi.com/api/amiibo";
-async function getData(money) {
-    try {
-        const response = await fetch(money);
-        const data = await response.json(); //makes the data into JSON object we can use
-        console.log(data)
-    } catch (error) {
-         console.log(error);
+const Url = "https://www.amiiboapi.com/api/amiibo";
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json(); //makes the data into JSON object we can use
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+async function Amiibos() {
+    const amiiboData = await getData(Url);
+    if (amiiboData && amiiboData.amiibo) {
+     AddAmiibo(amiiboData.amiibo);
     }
-    };
-    const amiibo = getData(Url)
-; 
-
-
-
- function AddAmiibo() {
-       //dfw.forEach((mibo)=> {
-    document.querySelector(".flex-container").insertAdjacentHTML("beforeend",
-    `
+  };
+  Amiibos();
   
-    <div class="amiibocard">
-    <h1 class="amiiboname"> ${amiibo.character} <h1>
-    <img src="${amiibo.image}"  alt="" class="amiiboimage">
-    <h2 class="amiiboseries">${amiibo.amiiboSeries}<h2>  
-    <div>`
-    )};  
- console.log(AddAmiibo)
+
+function AddAmiibo(dfw) {
+  dfw.forEach((d) => {
+    document.querySelector(".flex-container").insertAdjacentHTML(
+      "beforeend",
+      `<div class="amiiboccard">
+<h1 class="amiiboname">${d.character}</h1>
+<img src= "${d.image}" alt="" class="ItemImage">
+<h2 class="amiiboseries">${d.amiiboSeries}</h2>
+</div>`
+    );
+  });
+};
 
 
-
-/* function Searchfor(a){
-    let result;
-    if (a=amiibo){
-    
-    } else {
-        result = 'Not Found'
-        console.log(result)
+/* function SearchforAmiibo(name, data) {
+  for (let i = 0; i < data.length; i++) {
+    if (name === data[i].character) {
+    return data[i];
     }
-}
+  }
+  return("Cannot Find That Amiibo");
+};
+const Match = SearchforAmiibo( DomSelectors.amiiboname.value);
 
-
-const Search=
-DomSelectors.form.addEventListener("submit",function(event){
-    event.preventDefault();
-    console.log(DomSelectors.amiiboname.value);
-
-DomSelectors.form.addEventListener("submit", function(){
-        (el)=> el.textContent = DomSelectors.h1s.value})}
-);   
-  */
+DomSelectors.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+  }); */
